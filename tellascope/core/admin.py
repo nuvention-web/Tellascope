8e870f195b8b58df0ac04ea159bb0ad80c88f958
duner <asduner@gmail.com>
2015-04-28 00:53:07 -0500

    add UARs to the admin, yay for hacky workarounds

--- tellascope/core/admin.py
+++ tellascope/core/admin.py
@@ -8,7 +8,18 @@ admin.site.register(Article)
 admin.site.register(Source)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(UserArticleRelationship)
 
 
 
+# class ArticleInline(admin.StackedInline):
+#     model = UserProfile
+#     fk_name = 'user'
+
+# class UserArticleRelationshipAdmin(admin.ModelAdmin):
+#     list_display = ['get_userprofile_name', 'email']
+#     list_select_related = True
+#     inlines = [
+#         UserProfileInline,
+#     ]
 
