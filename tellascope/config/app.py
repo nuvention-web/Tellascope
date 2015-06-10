19ed61815b0d0137c39e274988336e9d454b6847
duner <asduner@gmail.com>
2015-06-10 11:24:42 -0500

    moving final app into master via copy paste because im bad at git

---
+++ tellascope/config/app.py
@@ -0,0 +1,7 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tellascope.config.settings.development")
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
