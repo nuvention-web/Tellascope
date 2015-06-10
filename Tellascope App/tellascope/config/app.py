48e83bce63445e2a1e3213d4109cbcd8f86944ab
duner <asduner@gmail.com>
2015-06-10 11:04:13 -0500

    i may have messed evertyghin up we'll see

--- Tellascope App/tellascope/config/app.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tellascope.config.settings.development")
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
